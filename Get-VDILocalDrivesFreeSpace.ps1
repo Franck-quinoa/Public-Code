23e166b953f583af18fe9ed8719d13c57d075d21
Franck-quinoa <franck.bordes@quinoa-groupe.fr>
2024-01-29 11:28:59 +0100

    Remplacement de Where par Where-Object

--- Get-VDILocalDrivesFreeSpace.ps1
+++ Get-VDILocalDrivesFreeSpace.ps1
@@ -8,7 +8,7 @@
         $pwd = ConvertTo-SecureString $PC.pass -AsPlainText -Force
         $Identite = New-Object System.Management.Automation.PSCredential($PC.user,$pwd)
         Invoke-Command -ComputerName $PC.name -Credential $Identite -ScriptBlock { 
-            Get-WmiObject -Class win32_Logicaldisk | where {($_.DriveType -eq 3 -or $_.DriveType -eq 2) -and $_.FileSystem -ne $null -and $_.DeviceID -eq "E:" } | Select -Property @{Name = 'Volume';Expression = {$_.DeviceID -replace ":",""}}, @{Name = '%Free';Expression = { "{0:N0}%" -f (($_.FreeSpace/$_.Size) * 100) } }, @{Name = 'Go Free';Expression = {"{0:N0}Go" -f ($_.FreeSpace/1000000000)}}, @{Name = 'Total';Expression = {"{0:N0}Go" -f ($_.Size/1000000000)}}
+            Get-WmiObject -Class win32_Logicaldisk | Where-Object {($_.DriveType -eq 3 -or $_.DriveType -eq 2) -and $_.FileSystem -ne $null -and $_.DeviceID -eq "E:" } | Select -Property @{Name = 'Volume';Expression = {$_.DeviceID -replace ":",""}}, @{Name = '%Free';Expression = { "{0:N0}%" -f (($_.FreeSpace/$_.Size) * 100) } }, @{Name = 'Go Free';Expression = {"{0:N0}Go" -f ($_.FreeSpace/1000000000)}}, @{Name = 'Total';Expression = {"{0:N0}Go" -f ($_.Size/1000000000)}}
         }
     }
 }
