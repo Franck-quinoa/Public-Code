8797be52629afa76e9a44fe58d0c89653b7c55cd
Franck-quinoa <franck.bordes@quinoa-groupe.fr>
2024-02-15 11:35:59 +0100

    Création du fichier pour désactiver les shares.

---
+++ switchSmbSharesStatus.ps1
@@ -0,0 +1,10 @@
+﻿# Get status of rules in the group
+Get-NetFirewallRule -DisplayGroup "Partage de fichiers et d’imprimantes" | Group-Object enabled | Select-Object Name | FT
+if($args[0]){
+#Write-Host "un paramètre ! on passe à true !"
+Set-NetFirewallRule -DisplayGroup "Partage de fichiers et d’imprimantes" -Enabled True
+}
+else{
+#Write-Host "pas de paramètre, on passe à False !"
+Set-NetFirewallRule -DisplayGroup "Partage de fichiers et d’imprimantes" -Enabled False
+}
