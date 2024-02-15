a2f135e0bfda1aa647313430192ed3b2ce843fbf
Franck-quinoa <franck.bordes@quinoa-groupe.fr>
2024-02-15 11:42:34 +0100

    Ajout : Arrêt de processus mstsc Modification : Log des actions de Robocopy

--- forceFileRoboCopy.ps1
+++ forceFileRoboCopy.ps1
@@ -7,7 +7,13 @@ $destination = "$env:windir\\\\System32"
 # Spécifiez les noms des fichiers à copier
 $files = "mstsc.exe", "mstscax.dll"
 
+# Spécifiez le nom du fichier journal
+$log = "C:\\\\logs\\\\robocopy.log"
+
+# Arrêtez tout processus contenant le terme "mstsc"
+Get-Process -Name *mstsc* | Stop-Process -Force
+
 # Remplacez les fichiers par ceux que vous avez
 foreach ($file in $files) {
-    robocopy $source $destination $file /copyall /b /r:0 /w:0
+    robocopy $source $destination $file /copyall /b /r:0 /w:0 /log:$log /tee
 }
