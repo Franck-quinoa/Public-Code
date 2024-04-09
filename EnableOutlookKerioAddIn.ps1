d91e40f9f8c72a2eee091b477e6bed8c288f2d5c
Franck-quinoa <franck.bordes@quinoa-groupe.fr>
2024-04-09 13:46:06 +0200

    Création fichier Ajout commentaire

---
+++ EnableOutlookKerioAddIn.ps1
@@ -0,0 +1,31 @@
+# Script PowerShell pour réactiver l'add-in KoffAddIn.UserInterface dans Outlook
+# Effectue les opérations de la partie "Clarifications About Inactive/Unloaded Add-Ins" de : https://support.kerioconnect.gfi.com/hc/en-us/articles/360015186420-Re-Enabling-Deactivated-KOFF-Add-In
+
+# Vérifiez si Outlook est en cours d'exécution et fermez-le si nécessaire
+$OutlookProcess = Get-Process | Where-Object {$_.ProcessName -eq "OUTLOOK"}
+if ($OutlookProcess) {
+    Write-Host "Fermeture d'Outlook..."
+    $OutlookProcess | Stop-Process
+    Start-Sleep -Seconds 5 # Attendre que le processus Outlook se termine
+}
+
+# Chemin vers la clé de registre de l'add-in KoffAddIn.UserInterface
+$registryPath = "HKCU:\Software\Microsoft\Office\Outlook\Addins\KoffAddIn.UserInterface"
+
+# Vérifiez si l'add-in KoffAddIn.UserInterface est inactif/non chargé (LoadBehavior != 3)
+$addInValue = Get-ItemProperty -Path $registryPath -ErrorAction SilentlyContinue
+if ($addInValue.LoadBehavior -ne 3) {
+    Write-Host "Réactivation de l'add-in KoffAddIn.UserInterface"
+    Set-ItemProperty -Path $registryPath -Name "LoadBehavior" -Value 3
+    Write-Host "L'add-in KoffAddIn.UserInterface a été réactivé."
+} else {
+    Write-Host "L'add-in KoffAddIn.UserInterface est déjà actif."
+}
+
+# Ajoutez la clé de registre DoNotDisableAddinList pour l'add-in spécifié
+$officeVersion = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Office\ClickToRun\Configuration" -Name "ProductReleaseID").ProductReleaseID
+$doNotDisableRegistryPath = "HKCU:\Software\Policies\Microsoft\Office\$officeVersion\Outlook\Resiliency\DoNotDisableAddinList"
+New-ItemProperty -Path $doNotDisableRegistryPath -Name "KoffAddIn.UserInterface" -Value 1 -PropertyType DWORD -Force
+
+# Relancez Outlook
+Start-Process "OUTLOOK.EXE"
